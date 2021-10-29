c3625f9885d1c4de09fb901ecab03ec07a6fe245
AloisioVasconcelos <alovasc@gmail.com>
2021-10-29 20:36:35 -0300

    Update index.jsx

--- components/Body/index.jsx
+++ components/Body/index.jsx
@@ -183,7 +183,7 @@ function Body() {
 
           <div >
             <p className={Styles.centralMap}>
-              <iframe className={Styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3840.2827373838554!2d-47.89640288572318!3d-15.736175225716552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a398fcc35baab%3A0x63994bb0af3e39df!2sBiosphere!5e0!3m2!1spt-BR!2sbr!4v1631923773925!5m2!1spt-BR!2sbr" width="600" height="600" style={{ border: 0 }} allowFullScreen="" loading="lazy" />
+              <iframe defer className={Styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3840.2827373838554!2d-47.89640288572318!3d-15.736175225716552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a398fcc35baab%3A0x63994bb0af3e39df!2sBiosphere!5e0!3m2!1spt-BR!2sbr!4v1631923773925!5m2!1spt-BR!2sbr" width="600" height="600" style={{ border: 0 }} allowFullScreen="" loading="lazy" />
             </p>
           </div>
         </div>
